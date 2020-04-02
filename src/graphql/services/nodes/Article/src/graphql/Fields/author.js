eca2b168b8d10579b0e53cc4dc2c2a58fb77653b
Daniel Starns <danielstarns@hotmail.com>
2020-04-02 05:46:28 +0100

    Feat: att Article.comment inter-schema query

--- src/graphql/services/nodes/Article/src/graphql/Fields/author.js
+++ src/graphql/services/nodes/Article/src/graphql/Fields/author.js
@@ -1,17 +1,31 @@
-const {
-    Types: { ObjectId }
-} = require("mongoose");
+const gql = require("graphql-tag");
 
-function author(
-    { author: authorInstance },
-    args,
-    { injections: { DataLoaders } }
-) {
-    if (ObjectId.isValid(authorInstance)) {
-        return DataLoaders.authors.load(authorInstance);
+async function author(root, args, { injections: { execute } }) {
+    const { data, errors } = await execute(
+        gql`
+            query (id: String!) {
+                user(id: $id) {
+                     id
+                     image
+                     username
+                     bio
+                     email
+                     followedByViewer
+                }
+            }
+        `,
+        {
+            variables: {
+                id: root.author
+            }
+        }
+    );
+
+    if (errors && errors.length) {
+        throw new Error(errors[0].message);
     }
 
-    return DataLoaders.authors.load(authorInstance.id);
+    return data.user;
 }
 
 module.exports = author;
