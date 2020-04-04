d044b7226a91d9b50d8b96d9a115c024131f631c
Daniel Starns <danielstarns@hotmail.com>
2020-04-04 10:56:56 +0100

    Feat: add inter-schema query for Viewer.user

--- src/graphql/services/nodes/Article/src/graphql/Types/Viewer.js
+++ src/graphql/services/nodes/Article/src/graphql/Types/Viewer.js
@@ -1,6 +1,5 @@
 const { GraphQLType } = require("idio-graphql");
 const gql = require("graphql-tag");
-const { User } = require("../../../../models/index.js");
 
 const Viewer = new GraphQLType({
     name: "Viewer",
@@ -58,8 +57,33 @@ const Viewer = new GraphQLType({
 
             return data.articles;
         },
-        user: ({ user: { id } }) => {
-            return User.findById(id);
+        user: async (
+            { user: { id } },
+            args,
+            { user, injections: { execute } }
+        ) => {
+            const { data, errors } = await execute(
+                gql`
+                {
+                    user: userById(
+                       id: ${id}
+                    ) {
+                        id
+                        image
+                        username
+                        bio
+                        email
+                    }
+                }
+            `,
+                { variables: { id }, context: { user } }
+            );
+
+            if (errors && errors.length) {
+                throw new Error(errors[0].message);
+            }
+
+            return data.user;
         }
     }
 });
