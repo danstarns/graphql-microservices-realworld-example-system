f2b1e3d0fc5d51b69cc29f838e0380d22b7cfab7
Daniel Starns <danielstarns@hotmail.com>
2020-04-05 12:39:23 +0100

    Fix: use only 1 gateway for graphql test client

--- test/graphql.js
+++ test/graphql.js
@@ -1,28 +1,26 @@
 const { createTestClient } = require("apollo-server-testing");
 const { GraphQLGateway } = require("idio-graphql");
 const { ApolloServer } = require("apollo-server-express");
-const fs = require("fs");
+const locals = require("../src/graphql/locals/index.js");
 
-const path = require("path");
 const { NATS_URL } = require("../src/config.js");
 
+const gateway = new GraphQLGateway(
+    {
+        locals,
+        services: {
+            nodes: ["Comment", "Article"]
+        }
+    },
+    { nodeID: "conduit_gateway", transporter: NATS_URL, logLevel: "error" }
+);
+
+let schema;
+
 async function testClient({ user } = {}) {
-    const locals = require("../src/graphql/locals/index.js");
-
-    console.log(await fs.promises.readdir(path.dirname(locals.schemaGlobals)));
-    console.log(await fs.promises.readFile(locals.schemaGlobals));
-    console.log(JSON.stringify(locals, null, 2));
-    const gateway = new GraphQLGateway(
-        {
-            locals,
-            services: {
-                nodes: ["Comment", "Article"]
-            }
-        },
-        { nodeID: "conduit_gateway", transporter: NATS_URL }
-    );
-
-    const { schema } = await gateway.start();
+    if (!schema) {
+        ({ schema } = await gateway.start());
+    }
 
     const server = new ApolloServer({
         schema,
